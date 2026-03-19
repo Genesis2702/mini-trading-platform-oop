6f38979b6db690b91637eda883fbe40295e02f56
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-03-19 21:21:53 +0700

    add: risk analyzer class implementation

--- RiskAnalyzer.java
+++ RiskAnalyzer.java
@@ -4,27 +4,48 @@ public class RiskAnalyzer<T extends Instrument> {
     private final List<T> instruments = new ArrayList<>();
 
     public void add(T instrument) {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        instruments.add(instrument);
     }
 
     public double averageRisk() {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        double avgRiskScore = 0.0;
+        for (T instrument : instruments) {
+            avgRiskScore += instrument.riskScore();
+        }
+        return avgRiskScore / instruments.size();
     }
 
     public T highestRisk() {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        T highestInstrument = null;
+        double highestRiskScore = -1e9;
+        for (T instrument : instruments) {
+            if (instrument.riskScore() > highestRiskScore) {
+                highestInstrument = instrument;
+                highestRiskScore = instrument.riskScore();
+            }
+        }
+        return highestInstrument;
     }
 
     public T lowestRisk() {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        T lowestInstrument = null;
+        double lowestRiskScore = 1e9;
+        for (T instrument : instruments) {
+            if (instrument.riskScore() < lowestRiskScore) {
+                lowestInstrument = instrument;
+                lowestRiskScore = instrument.riskScore();
+            }
+        }
+        return lowestInstrument;
     }
 
     public List<T> getAboveRiskThreshold(double threshold) {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        List<T> aboveRiskThreshold = new ArrayList<>();
+        for (T instrument : instruments) {
+            if (instrument.riskScore() > threshold) {
+                aboveRiskThreshold.add(instrument);
+            }
+        }
+        return aboveRiskThreshold;
     }
 }
